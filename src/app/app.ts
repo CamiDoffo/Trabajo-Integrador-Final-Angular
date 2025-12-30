7f403bfc55194f9318244d079cb6bbb98b692cc9
CamiDoffo <116676956+CamiDoffo@users.noreply.github.com>
2025-12-30 03:43:21 -0300

    algunos errores

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatComponent } from './chat-component/chat-component';
-// Asegúrate de importar tu componente de lista de chats
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Requisito Angular 17 
-  imports: [RouterOutlet, ChatComponent], // Importante: agrega ChatComponent aquí
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
